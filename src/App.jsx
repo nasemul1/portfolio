78d6455490a29e00f9ad7a2a1a4d0647b836347f
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-06-01 14:31:47 +0600

    added first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import First from './first-page/First'
 import Navbar from './navbar/Navbar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <First />
     </>
   )
 }
---
+++ src/first-page/First.jsx
@@ -0,0 +1,37 @@
+import './First.css';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import image from '../assets/profile-pic(2).png';
+
+const First = () => {
+    const [text] = useTypewriter({
+        words: ['Web developer'],
+        loop: 0
+    })
+    return (
+        <>
+            <div className="main-area">
+                <div className="main-text">
+                    <p className="hi-text">Hi, I am</p>
+                    <h2 id="name">Md Nasemul Islam</h2>
+                    <h3>I am a <span>{ text }</span>
+                        <Cursor cursorColor='#8CC152' />
+                    </h3>
+                    <p className="bio">Highly motivated aspiring front-end developer with a strong grasp of HTML, CSS, and JavaScript seeks an entry-level position to leverage skills and contribute to a dynamic team.</p>
+                    <div className="hireMe-div">
+                        <div className="hireMe-box">
+                            <a href="#contact">Hire Me</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="main-image">
+                    <div className="img-back-box"></div>
+                    <div className="img-box">
+                        <img src={ image } alt="my image" id='front-img'/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+ 
+export default First;
